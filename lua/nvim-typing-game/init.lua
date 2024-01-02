578e9b66b159066739110973aa4ce85db4546af6
garchomp-game <otoka.japanesedrum@gmail.com>
2024-01-02 13:08:26 +0900

    commit

--- lua/nvim-typing-game/init.lua
+++ lua/nvim-typing-game/init.lua
@@ -4,13 +4,20 @@ local ui_popup = require("nvim-typing-game.ui.popup")
 
 local input_popup
 local text_popup
+local current_line = 1
 
 local function on_input_submit(value)
-  game_core.process_input(value)
+  local is_correct = game_core.process_input(value)
+  if is_correct then
+    current_line = current_line + 1
+    text_popup:unmount()  -- 現在のテキストポップアップを閉じる
+    text_popup = ui_popup.show_text_popup(current_line, game_core.get_registered_words())
+  end
+
   if game_core.is_game_over() then
     input_popup:unmount()
     text_popup:unmount()
-    -- ここでゲーム終了時の処理を行う
+    -- ゲーム終了時の処理
   end
 end
 
@@ -19,7 +26,7 @@ local M = {}
 function M.start_game()
   local lines = vim.api.nvim_buf_get_lines(0, vim.api.nvim_win_get_cursor(0)[1] - 1, -1, false)
   game_core.init_game(lines)
-  text_popup = ui_popup.show_text_popup(lines)
+  text_popup = ui_popup.show_text_popup(current_line, lines)
   input_popup = ui_popup.show_input_popup(on_input_submit)
 end
 
--- lua/nvim-typing-game/ui/popup.lua
+++ lua/nvim-typing-game/ui/popup.lua
@@ -6,8 +6,8 @@ local M = {}
 
 function M.show_input_popup(on_input_submit)
   local input_popup = nui_input({
-    position = "50%",
-    size = { width = 20 },
+    position = "20%",
+    size = { width = 50 },
     border = { style = "rounded" },
     win_options = { winhighlight = "Normal:Normal,FloatBorder:Teal" },
   }, {
@@ -19,18 +19,28 @@ function M.show_input_popup(on_input_submit)
   return input_popup
 end
 
-function M.show_text_popup(text_lines)
+-- ui/popup.lua の更新部分
+function M.show_text_popup(current_line, text_lines)
+  local screen_height = vim.api.nvim_win_get_height(0)
+  local popup_height = math.floor(screen_height * 0.5)
   local text_popup = Popup({
     position = "20%",
-    size = { width = 50, height = #text_lines },
+    size = { width = 50, height = popup_height },
     border = { style = "rounded" },
   })
 
   text_popup:mount()
   local text_buf = text_popup.bufnr
+
   vim.api.nvim_buf_set_lines(text_buf, 0, -1, false, text_lines)
   vim.api.nvim_set_option_value('modifiable', false, {buf = text_buf})
   vim.api.nvim_set_option_value('readonly', true, {buf = text_buf})
+
+  -- ハイライトを適用
+  for i, _ in ipairs(text_lines) do
+    local highlight_group = (i == current_line) and "Normal" or "Comment"
+    vim.api.nvim_buf_add_highlight(text_buf, -1, highlight_group, i - 1, 0, -1)
+  end
 
   return text_popup
 end
