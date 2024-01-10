772ae25aef6d6c86be9f78bcc0308c74dddc1d0e
garchomp-game <otoka.japanesedrum@gmail.com>
2024-01-10 10:09:26 +0900

    commit

--- plugin/nvim-typing-game.lua
+++ plugin/nvim-typing-game.lua
@@ -4,15 +4,16 @@ vim.api.nvim_create_user_command('TypingGameStart', typing_game.start_game, {})
 require('mini.doc').setup({
 })
 
+local home = vim.env.HOME
 -- Luaファイルのパスを指定
 local input_files = {
-  '/home/garchomp-game/.config/nvim/pack/nvim-typing-game/lua/nvim-typing-game/init.lua',
-  '/home/garchomp-game/.config/nvim/pack/nvim-typing-game/lua/nvim-typing-game/core/game.lua',
-  '/home/garchomp-game/.config/nvim/pack/nvim-typing-game/lua/nvim-typing-game/ui/popup.lua'
+  home .. '/.config/nvim/pack/nvim-typing-game/lua/nvim-typing-game/init.lua',
+  home .. '/.config/nvim/pack/nvim-typing-game/lua/nvim-typing-game/core/game.lua',
+  home .. '/.config/nvim/pack/nvim-typing-game/lua/nvim-typing-game/ui/popup.lua'
 }
 
 -- 出力ファイルのパスを指定
-local output_file = '/home/garchomp-game/.config/nvim/pack/nvim-typing-game/doc/nvim-typing-game-help.txt'
+local output_file = home .. '/.config/nvim/pack/nvim-typing-game/doc/nvim-typing-game-help.txt'
 
 -- ドキュメント生成
 require('mini.doc').generate(input_files, output_file)
